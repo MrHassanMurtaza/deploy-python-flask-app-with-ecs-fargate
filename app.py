9d2c7d70a656afa81880bc90eb3c3510d6f675ae
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-06-20 13:56:45 +0500

    Exception handling + comment
    Added exception handling + comment for template folder

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 
+# template_folder points to current directory. Flask will look for '/static/'
 app = Flask(__name__, template_folder='.')
 # The rest of your file here
 
 @app.route('/')
 def index():
   """ Serving static files """
-  return render_template('index.html')
-
+  try:
+    return render_template('index.html')
+  except:
+    return str(e)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=5000)
